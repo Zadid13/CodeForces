7e6566b84a63e683f0ffde64c96c12567117cd1a
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-05-30 00:20:32 +0600

    Update 118A.cpp

--- 118A.cpp
+++ 118A.cpp
@@ -30,3 +30,44 @@ int main()
     cout << t << endl;
     return 0;
 }
+
+// Another Implementation
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+
+bool isVowel(char c)
+{
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+}
+
+int main()
+{
+    optimize();
+
+    string s, ans;
+
+    cin >> s;
+
+    for (auto u : s)
+    {
+        char c = tolower(u);
+
+        if (!isVowel(c))
+        {
+            ans += '.';
+            ans += c;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+
+*/
