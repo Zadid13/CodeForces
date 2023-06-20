df3e0c5e8b0dedbfd42701b38fbe9580ed125862
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-06-21 02:01:00 +0600

    Add files via upload

---
+++ 1843A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl "\n"
+#define pb push_back
+#define ll long long
+
+int main()
+{
+    optimize();
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        int sum = 0;
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            sum += (v[r] - v[l]);
+            l++, r--;
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
