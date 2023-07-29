5724a6433b171c1abf1c6636258302eedb0e1918
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-07-29 14:29:51 +0600

    Add files via upload

---
+++ 208A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl "\n"
+#define pb push_back
+#define ll long long
+
+int main()
+{
+    optimize();
+
+    string s;
+    int flag = 1;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+            i += 2;
+            if (!flag)
+            {
+                cout << " ";
+            }
+            continue;
+        }
+        else
+        {
+            flag = 0;
+            cout << s[i];
+        }
+    }
+
+    return 0;
+}
