665bf27fa884c0689b240159ad40afbfe9a6a2b2
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-10-06 23:45:55 +0600

    Add files via upload

---
+++ 1863A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl "\n"
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(vec) vec.begin(), vec.end()
+#define ins insert
+#define ld long double
+#define ll long long
+
+int main() {
+    optimize();
+    int t;
+    cin >> t;
+    while (t--) {
+        ll total, online, n;
+        cin >> total >> online >> n;
+        string s;
+        ll check = 0;
+        cin >> s;
+        bool flag = false;
+
+        ll online_subscribers = online;
+        
+            for (ll i = 0; i < n; i++)
+            
+                {
+                if (online>=total)
+                {
+                    check = 1;
+                }
+                
+                if (s[i] == '-') {
+                online--;}
+                else
+                {
+                    online++;
+                    online_subscribers++;
+                }
+                if (online>=total)
+                {
+                    check =1 ;
+                }
+                
+                
+            }
+            if (online_subscribers>=total && check == 0)
+            {
+                check = 2;
+
+            }
+            
+        if (check == 1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else if (check == 2)
+        {
+            cout<<"MAYBE"<<endl;
+        }
+        else{
+        cout<<"NO"<<endl;
+                }
+        
+
+        
+    }
+
+    return 0;
+}
