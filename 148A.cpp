d11f19aac133dacc21e2619b5cb52a48af3a5e70
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2024-03-20 23:13:13 +0600

    Add files via upload

---
+++ 148A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io()                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define all(vec) vec.begin(), vec.end()
+#define ins insert
+#define ld long double
+#define ll long long
+
+int main()
+{
+    fast_io();
+    ll k, l, m, n, d, count = 0;
+    cin >> k >> l >> m >> n >> d;
+    for (ll i = 1; i <= d; i++)
+    {
+        if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
