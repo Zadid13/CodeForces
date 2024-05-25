2b02f677adc8471e754b8b17aff7b2a565782c49
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2024-05-26 04:54:56 +0600

    Add files via upload

---
+++ 1950A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io()                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define pb push_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define all(vec) vec.begin(), vec.end()
+#define ins insert
+#define ld long double
+#define ll long long
+
+int main()
+{
+    fast_io();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        // Place your code here
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a < b && b < c)
+        {
+            cout << "STAIR" << endl;
+        }
+        else if (a < b && b > c)
+        {
+            cout << "PEAK" << endl;
+        }
+        else
+        {
+            cout << "NONE" << endl;
+        }
+    }
+    return 0;
+}
